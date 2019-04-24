9f4a79915d50529c8031a4fec42c5d0658451867
Seil Cho <seilcho7@hotmail.com>
2019-04-24 13:25:50 -0400

    add restaurants.js

---
+++ src/restaurants.js
@@ -0,0 +1,37 @@
+
+
+const restaurants = {
+    "mediterranean": {
+        "Mediterranean Grill": [
+            "hummus", "tabouli", "dolmas"
+        ],
+        "Yalla": [
+            "hummus wrap", "tabouli wrap", "dolmas"
+        ],
+        "Nik's": [
+            "spanakopita", "gyro", "dolmas"
+        ]
+    },
+    "american": {
+        "Chik fil a": [
+            "chicken biscuit", "chicken sandwich", "chicken salad"
+        ],
+        "Mary Mac's Tea Room": [
+            "biscuit", "fried chicken", "mashed potatoes"
+        ],
+        "Jersey Mike's": [
+            "Big Sandwich", "Bigger Sandwich", "Most Biggerest Sandwich"
+        ]
+    },
+    "mexican": {
+        "Elmyriachi": [
+            "tacos", "burritos", "cheese dip"
+        ],
+        "Mezcalito's Cantina": [
+            "tortas", "tamales", "cheese dip"
+        ],
+        "Matador": [
+            "cheese dip", "cheese dip", "cheese dip"
+        ]
+    }
+}
